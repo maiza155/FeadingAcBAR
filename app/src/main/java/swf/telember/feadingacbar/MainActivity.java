5d428779f01420909d4e0628d2db9a5118fd83b5
kittipongBNB SWF <cartearo@gmail.com>
2015-01-22 08:53:39 +0700

    Initial commit

---
+++ app/src/main/java/swf/telember/feadingacbar/MainActivity.java
@@ -0,0 +1,115 @@
+package swf.telember.feadingacbar;
+
+
+import android.content.res.TypedArray;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+
+import com.nineoldandroids.view.ViewHelper;
+
+
+public class MainActivity extends ActionBarActivity implements ObservableScrollViewCallbacks {
+
+
+    private View mFlexibleSpaceView;
+    private View mToolbarView;
+    private TextView mTitleView;
+    private int mFlexibleSpaceHeight;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mFlexibleSpaceView = findViewById(R.id.flexible_space);
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText(getTitle());
+        setTitle(null);
+        mToolbarView = findViewById(R.id.toolbar);
+
+        final ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        scrollView.setScrollViewCallbacks(this);
+
+        mFlexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_height);
+        int flexibleSpaceAndToolbarHeight = mFlexibleSpaceHeight + getActionBarSize();
+
+        findViewById(R.id.body).setPadding(0, flexibleSpaceAndToolbarHeight, 0, 0);
+        mFlexibleSpaceView.getLayoutParams().height = flexibleSpaceAndToolbarHeight;
+
+        ScrollUtils.addOnGlobalLayoutListener(mTitleView, new Runnable() {
+            @Override
+            public void run() {updateFlexibleSpaceText(scrollView.getCurrentScrollY());
+            }
+        });
+    }
+
+
+     int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean b, boolean b2) {
+        updateFlexibleSpaceText(scrollY);
+
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+
+    }
+    private void updateFlexibleSpaceText(final int scrollY) {
+        ViewHelper.setTranslationY(mFlexibleSpaceView, -scrollY);
+        int adjustedScrollY = scrollY;
+        if (scrollY < 0) {
+            adjustedScrollY = 0;
+        } else if (mFlexibleSpaceHeight < scrollY) {
+            adjustedScrollY = mFlexibleSpaceHeight;
+        }
+        float maxScale = (float) (mFlexibleSpaceHeight - mToolbarView.getHeight()) / mToolbarView.getHeight();
+        float scale = maxScale * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight;
+
+        ViewHelper.setPivotX(mTitleView, 0);
+        ViewHelper.setPivotY(mTitleView, 0);
+        ViewHelper.setScaleX(mTitleView, 1 + scale);
+        ViewHelper.setScaleY(mTitleView, 1 + scale);
+        ViewHelper.setTranslationY(mTitleView, ViewHelper.getTranslationY(mFlexibleSpaceView) + mFlexibleSpaceView.getHeight() - mTitleView.getHeight() * (1 + scale));
+        int maxTitleTranslationY = mToolbarView.getHeight() + mFlexibleSpaceHeight - (int) (mTitleView.getHeight() * (1 + scale));
+        int titleTranslationY = (int) (maxTitleTranslationY * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight);
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+    }
+}
